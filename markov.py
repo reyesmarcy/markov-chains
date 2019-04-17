8c0e79ab88d854bea0e0107e684fdef89d04e02d
Hackbright Student <no-reply@hackbrightacademy.com>
2019-04-16 17:46:24 -0700

    Added new function so that you could enter as many text files on command line as you want.

--- markov.py
+++ markov.py
@@ -1,6 +1,7 @@
 """Generate Markov text from text files."""
 
 import random
+import sys
 
 
 def open_and_read_file(file_path):
@@ -16,6 +17,29 @@ def open_and_read_file(file_path):
     # print(file)
 
     return file
+
+
+def combine_files():
+
+    files_count = len(sys.argv) 
+
+    all_text = ""
+
+    i = 1 
+
+    while i < len(sys.argv):
+    # for i in range(files_count):
+
+        all_text = all_text + open_and_read_file(sys.argv[i])
+
+        i += 1
+
+    #     file1_text = open_and_read_file(file_path1)
+    #     file2_text = open_and_read_file(file_path2)
+
+    # all_text = file1_text + file2_text
+
+    return all_text
 
 
 def make_chains(text_string):
@@ -78,23 +102,35 @@ def make_text(chains):
     sentence_words.append(second_word)
 
     # While the current key is a valid key
-    while current_key in chains: 
+
+    # uncomment to control length
+    sentence_length = len(sentence_words)
+    while current_key in chains and sentence_length < 200: 
+
+    # while current_key in chains: 
     # Randomly select next word and add to list of sentence words
         next_word = random.choice(chains[current_key])
         sentence_words.append(next_word)
         # Update current key to use for lookup 
         current_key = (current_key[1], next_word)
 
+        sentence_length = len(sentence_words) # uncomment for control length
+
     # Turn the list into a string
     return " ".join(sentence_words)
 
 
 # input_path = "gettysburg.txt"
-input_path = "green-eggs.txt"
+# input_path = "green-eggs.txt"
 # input_path = "blank_space.txt"
 
+# input_path1 = sys.argv[1]
+# input_path2 = sys.argv[2]
+
 # Open the file and turn it into one long string
-input_text = open_and_read_file(input_path)
+# input_text = open_and_read_file(input_path)
+
+input_text = combine_files()
 
 # Get a Markov chain
 chains = make_chains(input_text)
