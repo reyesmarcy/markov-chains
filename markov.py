85ec2d88354b0051e62513a3059fba94394711c4
Hackbright Student <no-reply@hackbrightacademy.com>
2019-04-17 12:48:14 -0700

    Add removal of non sentence-ending chars

--- markov.py
+++ markov.py
@@ -12,9 +12,16 @@ def open_and_read_file(file_path):
     """
 
     # your code goes here
+    file = open(file_path).read()
+    bad_characters = ["@", ",", ":", ";","--", "-", "*","(", ")", "^", "+", "_", "~"]
+    
+    sentence_ending_punctuation = [".", "?", "!"]
 
-    file = open(file_path).read().replace("\n", " ")
-    # print(file)
+    for char in bad_characters:
+        file = file.replace(char, "")
+        print("replaced:", char)
+
+
 
     return file
 
@@ -67,47 +74,19 @@ def make_chains(text_string, n = 2):
         [None]
     """
 
-    # thoughts for tomorrow:
-
-    # change key to be of n length
-    # change third word to be at the n+1 index
-
-
-
     # split text string into list of words
     words = text_string.split()
 
-    # loop over list of words by index thru len - 1
-
+    # loop over list of words by index thru len - n
     chains = {}
 
-    for i in range(len(words) - n): # change to - n
-
-        # consider slice to make key tuple
-
-        # (words[i],....,words[i+n])
-
-        # first_word = words[i]
-        # second_word = words[i + 1]
-        # third_word = words[i + 2]
-        # fourth_word = words[i + 3]
-
+    for i in range(len(words) - n):
+        # Used slicing to create a n-gram tuple key from i:i+(n-1)
         tuple_key = (tuple(words[i:(i + n)]))
-
+        # Get the word at the nth index to add to the keys' corresponding list of values
         word_to_add_to_list = words[i + n]
 
-        # first_word = words[i]
-        # second_word = words[i+1]
-
-        # pair = (tuple(words[i:i+2]))
-
-        # pair = (first_word, second_word)
-
-        # third_word = words[i+2]
-
-        # chains[pair] = chains.get(pair, [])
-        # chains[pair].append(third_word)
-
+        # Add keys and values to dictionary 
         chains[tuple_key] = chains.get(tuple_key, [])
         chains[tuple_key].append(word_to_add_to_list)
 
@@ -115,44 +94,48 @@ def make_chains(text_string, n = 2):
     return chains
 
 
-def make_text(chains):
+def make_text(chains, output_length = 200):
     """Return text from chains."""
     # Create empty list to add words to 
-    sentence_words = []
+    # sentence_words = []
 
     # We are adding to the list, a randomly selected pair of words from the 
     # chains dictionary keys to start our new sentence 
     current_key = random.choice(list(chains.keys()))
-    first_word, second_word = current_key
 
-    sentence_words.append(first_word)
-    sentence_words.append(second_word)
+    print(current_key)
+    print()
+
+    sentence_words = list(current_key)
+
+    # first_word, second_word = current_key
+
+    # sentence_words.append(first_word)
+    # sentence_words.append(second_word)
 
     # While the current key is a valid key
 
     # uncomment to control length
     sentence_length = len(sentence_words)
-    while current_key in chains and sentence_length < 200: 
+    while current_key in chains and sentence_length < output_length: 
 
     # while current_key in chains: 
     # Randomly select next word and add to list of sentence words
         next_word = random.choice(chains[current_key])
         sentence_words.append(next_word)
-        # Update current key to use for lookup 
-        current_key = (current_key[1], next_word)
 
+        # Update current key to use for lookup 
+        current_key = list(current_key[1:])
+        current_key.append(next_word)
+        current_key = tuple(current_key)
+        
+        print(current_key)
+        print()
         sentence_length = len(sentence_words) # uncomment for control length
 
     # Turn the list into a string
     return " ".join(sentence_words)
 
-
-# input_path = "gettysburg.txt"
-# input_path = "green-eggs.txt"
-# input_path = "blank_space.txt"
-
-# input_path1 = sys.argv[1]
-# input_path2 = sys.argv[2]
 
 # Open the file and turn it into one long string
 # input_text = open_and_read_file(input_path)
@@ -160,9 +143,10 @@ def make_text(chains):
 input_text = combine_files()
 
 # Get a Markov chain
-chains = make_chains(input_text, 4)
-print(chains)
+chains = make_chains(input_text, 3)
+# print(chains)
+print()
 # # Produce random text
-# random_text = make_text(chains)
+random_text = make_text(chains)
 
-# print(random_text)
+print(random_text)
